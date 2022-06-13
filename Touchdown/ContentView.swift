16945fb3fb459ec111d58580ddfb9d874fb3f423
Eren Kara <erenkaraaa47@gmail.com>
2022-06-13 12:42:13 +0300

    Initial Commit

---
+++ Touchdown/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Touchdown
+//
+//  Created by user on 13.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by user on 13.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
